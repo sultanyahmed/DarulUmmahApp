31cfd5e21a22b81873b0a175f5651aa6debbcaab
Sultan Yousuf Ahmed <sultanyousufahmed@Mac.communityfibre.co.uk>
2026-04-29 11:32:08 +0100

    Clean shared UI and Xcode project warnings

--- composeApp/src/commonMain/kotlin/com/example/darulummahapp/App.kt
+++ composeApp/src/commonMain/kotlin/com/example/darulummahapp/App.kt
@@ -28,6 +28,7 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuAnchorType
 import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
@@ -1873,7 +1874,10 @@ private fun AddAnnouncementCard(
                             ExposedDropdownMenuDefaults.TrailingIcon(expanded = startTimeMenuExpanded)
                         },
                         modifier = Modifier
-                            .menuAnchor()
+                            .menuAnchor(
+                                type = ExposedDropdownMenuAnchorType.PrimaryNotEditable,
+                                enabled = true,
+                            )
                             .fillMaxWidth(),
                         colors = announcementFieldColors(),
                         singleLine = true,
@@ -1917,7 +1921,10 @@ private fun AddAnnouncementCard(
                             ExposedDropdownMenuDefaults.TrailingIcon(expanded = timeMenuExpanded)
                         },
                         modifier = Modifier
-                            .menuAnchor()
+                            .menuAnchor(
+                                type = ExposedDropdownMenuAnchorType.PrimaryNotEditable,
+                                enabled = true,
+                            )
                             .fillMaxWidth(),
                         colors = announcementFieldColors(),
                         singleLine = true,
