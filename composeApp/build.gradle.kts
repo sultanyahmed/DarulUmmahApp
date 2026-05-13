0d5168e9a6e7b1a0d861d0e4b915fc203211d963
Sultan Yousuf Ahmed <sultanyousufahmed@Mac.communityfibre.co.uk>
2026-05-13 14:17:11 +0100

    Increment Android bundle version code

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -90,7 +90,7 @@ android {
         applicationId = "com.sultanyahmed.darulummahapp"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0.1"
         buildConfigField(
             "String",
