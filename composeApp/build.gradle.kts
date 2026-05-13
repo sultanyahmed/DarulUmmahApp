a790f3df0131da20f05d775753b3688dda8132d2
Sultan Yousuf Ahmed <sultanyousufahmed@Mac.communityfibre.co.uk>
2026-05-13 14:12:49 +0100

    Bump Android release version

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -90,8 +90,8 @@ android {
         applicationId = "com.sultanyahmed.darulummahapp"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
         buildConfigField(
             "String",
             "SUPABASE_URL",
